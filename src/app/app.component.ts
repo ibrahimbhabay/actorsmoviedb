7d6de2498f508f2c3a3908edfe06f9cf33b7cd31
ibha0002 <ibrahim.bhabay@gmail.com>
2020-10-16 03:06:29 +1100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'w8ang';
+  title = 'w8ang'
+  car = {
+    id: '',
+    maker: "",
+    model: "",
+    year: "",
+    address: "",
+    state: "",
+    postCode: "",
+    bodyType: ""
+  }
+
+  db = [];
+
+  saveCar() {
+    this.car.id = uuid.v4();
+    this.db.push(this.car);
+    this.car = Object.create(this.car);
+  }
+
+  deleteCar(id) {
+    console.log(id);
+    for (let i = 0; i < this.db.length; i++) {
+      if (this.db[i].id === id) {
+        this.db.splice(i, 1);
+        break;
+      }
+    }
+  }
+  getOldCarsNumber() {
+    let counter = 0;
+
+    this.db.forEach(car => {
+      if (car.year < 2000)
+        counter++;
+    })
+
+    return counter;
+  }
+
+  deleteOldCars() {
+    let i = 0;
+    while (i < this.db.length) {
+      if (this.db[i].year < 2000)
+        this.db.splice(i, 1);
+      else i++;
+
+    }
+
+  }
+
 }
